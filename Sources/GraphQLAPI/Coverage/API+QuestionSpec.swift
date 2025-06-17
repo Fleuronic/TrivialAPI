22e00df126d53534a0a5c422154e9a6010618374
Jordan Kay <jordan@fleuronic.com>
2025-06-16 23:32:31 -0400

    Update spec conformance

--- Sources/GraphQLAPI/Coverage/API+QuestionSpec.swift
+++ Sources/GraphQLAPI/Coverage/API+QuestionSpec.swift
@@ -5,10 +5,24 @@ import struct Trivial.Category
 import protocol Catena.Scoped
 import protocol TrivialService.QuestionSpec
 
+import Catenary
+
 extension API: QuestionSpec {
 	#if swift(<6.0)
 	public typealias QuestionListFields = QuestionSpecifiedFields
 	#endif
+
+    public func fetchQuestion(with id: Question.ID) async -> SingleResult<QuestionSpecifiedFields> {
+        await result {
+            let response: Response<QuestionSpecifiedFields> = try await endpoint.run(
+                Query<Self, _>(
+                    Where(\Question.Identified.id, Equals(id))
+                )
+            )
+
+            return try response.fields.first!
+        }
+    }
 
     public func listQuestions(of type: Question.QuestionType? = nil, inCategoryWith categoryID: Category.ID? = nil, with difficulty: Question.Difficulty? = nil, count: Int? = nil) async -> Results<QuestionSpecifiedFields> {
 		await result {
